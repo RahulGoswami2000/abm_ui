ebdad0d55de6d1fcce709f42ddcbc1888c9927a1
Rahul Goswami <rh917388@dal.ca>
2024-12-13 14:45:34 -0800

    changed

--- src/Pages/UserTask/UserTask.tsx
+++ src/Pages/UserTask/UserTask.tsx
@@ -277,7 +277,7 @@ const ImageHoverTracker: React.FC = () => {
       hoverTimeImage1 / 360
     } seconds on the weed image and ${
       hoverTimeImage2 / 360
-    } seconds on the neutral image. Provide feedback on engagement and how they can cure there anxiety if they are looking at negative aspect. And make it short please`;
+    } seconds on the neutral image. Provide feedback on engagement and how they can cure there anxiety if they are looking at negative aspect. And make it just two line please`;
 
     try {
       const token = localStorage.getItem("token");
